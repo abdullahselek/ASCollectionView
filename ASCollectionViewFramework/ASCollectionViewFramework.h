5f5c118bc25853575d8b73a8a0800f11a94c2a72
abdullahselek <abdullahselek@gmail.com>
2016-12-04 21:03:49 +0100

    - prepare for carthage support

---
+++ ASCollectionViewFramework/ASCollectionViewFramework.h
@@ -0,0 +1,19 @@
+//
+//  ASCollectionViewFramework.h
+//  ASCollectionViewFramework
+//
+//  Created by Abdullah Selek on 04/12/2016.
+//  Copyright © 2016 Abdullah Selek. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ASCollectionViewFramework.
+FOUNDATION_EXPORT double ASCollectionViewFrameworkVersionNumber;
+
+//! Project version string for ASCollectionViewFramework.
+FOUNDATION_EXPORT const unsigned char ASCollectionViewFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ASCollectionViewFramework/PublicHeader.h>
+
+
